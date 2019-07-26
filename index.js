bf01f14d2ee88816a72b88018d552ae0d93317e4
Vibhore Mohnot <45279814+vibhoremohnot72@users.noreply.github.com>
2019-07-27 04:57:49 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const {token, prefix} = require('./config.json');
+const {token, prefix} = require('./config.json')
 const client = new Discord.Client();
 
 client.once('ready', () => {
