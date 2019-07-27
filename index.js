c023d8c93b5d0fcb1258412b6c1cfca15647fe17
Vibhore Mohnot <45279814+vibhoremohnot72@users.noreply.github.com>
2019-07-27 11:06:50 +0530

    commit

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ client.on('message', message => {
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		client.commands.get('ping').execute(message, args);
+	}
 });
 
 client.login(token);
